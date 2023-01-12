0b59e18683123109d21d29f6f5d1142f84b8a29d
John M <john.r.martin@colorado.edu>
2023-01-12 15:51:45 -0700

    Organize file formatting functions

--- GravityModels/CelestialBodies/Asteroids.py
+++ GravityModels/CelestialBodies/Asteroids.py
@@ -46,98 +46,14 @@ class Bennu:
             processor=remove_whitespace
         )
 
-
-        # Spherical Harmonics
-        def format_sh(fname, action, pooch_inst):
-            new_name = fname.split("_raw.txt")[0] + ".txt"
-            if os.path.exists(new_name):
-                return new_name
-            # Pull data from the .m file
-            with open(fname, 'r') as f:
-                data = f.readlines()
-            
-            for i in range(len(data)):
-                line = data[i]
-                if "DEGREE" in line:
-                    max_deg = int(line.split("=")[1].split(";")[0])
-                if "NAMES" in line:
-                    name_start_idx = i+1
-                if "};" in line:
-                    if "BENNU" in line:
-                        name_end_idx = i+1
-                    else:
-                        name_end_idx = i
-                if "VALS" in line:
-                    values_start_idx = i+1
-                if "];" in line:
-                    if "-" in line:
-                        values_end_idx = i+1
-                    else:
-                        values_end_idx = i
-                    break
-
-            names = data[name_start_idx:name_end_idx]
-            values = data[values_start_idx:values_end_idx]
-            
-            # Gather keys and values
-            name_list = []
-            value_list = []
-            for i in range(len(names)):
-                name_entries = [entry.replace(" ","").replace("'", "").replace("BENNU_", "").replace("...", "") for entry in names[i].split("' '")]
-                value_entries = values[i].split(" ")
-                value_entries = np.unique(value_entries).tolist()
-                value_entries.remove("")
-                try:
-                    value_entries.remove('...\n')#[3:-1]
-                except:
-                    value_entries.remove('];\n')
-                name_list = np.concatenate((name_list, name_entries))
-                value_list = np.concatenate((value_list, value_entries))
-
-            # Standardize formatting and populate data array
-            data_matrix = np.zeros((max_deg*(max_deg+1) - 2*(2+1), 6))
-            for k in range(len(name_list)):
-                name = name_list[k]
-                if "J" in name:
-                    degree = name.split("J")[1]
-                    if len(degree) > 1:
-                        name_list[k] = "C" + degree + "00"
-                    else:
-                        name_list[k] = "C0" + degree + "00"
-                if "GM" in name:
-                    continue
-                
-                coef = name_list[k][0]
-                i = int(name_list[k][1:3])
-                j = int(name_list[k][3:5])
-                value = float(value_list[k])
-
-                if coef == "C":
-                    data_matrix[(i-2)*max_deg + j] = np.array([i, j, value, data_matrix[(i-2)*max_deg + j, 3], 0.0, 0.0])
-                else:
-                    data_matrix[(i-2)*max_deg + j] = np.array([i, j, data_matrix[(i-2)*max_deg + j, 2], value, 0.0, 0.0])
-
-            # Remove empty rows 
-            data_matrix=data_matrix[~np.all(data_matrix == np.array([0,0,0,0,0,0]),axis=1)]
-
-            # Write data to processed file
-            with open(new_name, 'w') as f:
-                f.write("    %f    %f    %f    %d\n" % (self.radius, self.mu, 0.0, 16))
-                f.write("    0\t0\t1.00000000000E+00\t0.00000000000E+00\t0.00000000000E+00\t0.00000000000E+00\n")
-                f.write("    1\t0\t0.00000000000E+00\t0.00000000000E+00\t0.00000000000E+00\t0.00000000000E+00\n")
-                f.write("    1\t1\t0.00000000000E+00\t0.00000000000E+00\t0.00000000000E+00\t0.00000000000E+00\n")
-                for row in data_matrix:
-                    f.write("\t%d\t%d\t%e\t%e\t%e\t%e \n" % (row[0], row[1], row[2], row[3],row[4],row[5]))
-            return new_name
-
         # https://ssd.jpl.nasa.gov/tools/gravity.html#/bennu 
         #grav_20_particles.m - A MATLAB script that provides the coefficients and covariance of the estimated gravity field.
         self.sh_10 = pooch.retrieve(
             url='https://figshare.com/ndownloader/files/21927342',
             known_hash="d002eb615caab83665d991ecaa43480b85569e7f830f4aac9b2824d04d7b0dea",
             fname="Bennu_sh_10_raw.txt",
-            path=os.path.dirname(GravityModels.__file__) + "/Files/GravityModels/Bennu/",
-            processor=format_sh
+            path=os.path.dirname(GravityModels.__file__) + "/Files/SH_coefficients/Bennu/",
+            processor=self.format_sh
         )
 
         # grav_shape_16x16.m - A MATLAB script that provides the coefficients of the shape-based uniform density gravity field.
@@ -145,11 +61,97 @@ class Bennu:
             url='https://figshare.com/ndownloader/files/21927351',
             known_hash="857cd603f9a9b42f60562ec19b60ac095a16005c3f1b5bd85b16436c65e5e35b",
             fname="Bennu_sh_shape_16_raw.txt",
-            path=os.path.dirname(GravityModels.__file__) + "/Files/GravityModels/Bennu/",
-            processor=format_sh
+            path=os.path.dirname(GravityModels.__file__) + "/Files/SH_coefficients/Bennu/",
+            processor=self.format_sh
         )
 
         self.sh_file = self.sh_10
+
+
+    # Spherical Harmonics
+    def format_sh(self, fname, action, pooch_inst):
+        new_name = fname.split("_raw.txt")[0] + ".txt"
+        if os.path.exists(new_name):
+            return new_name
+        # Pull data from the .m file
+        with open(fname, 'r') as f:
+            data = f.readlines()
+        
+        for i in range(len(data)):
+            line = data[i]
+            if "DEGREE" in line:
+                max_deg = int(line.split("=")[1].split(";")[0])
+            if "NAMES" in line:
+                name_start_idx = i+1
+            if "};" in line:
+                if "BENNU" in line:
+                    name_end_idx = i+1
+                else:
+                    name_end_idx = i
+            if "VALS" in line:
+                values_start_idx = i+1
+            if "];" in line:
+                if "-" in line:
+                    values_end_idx = i+1
+                else:
+                    values_end_idx = i
+                break
+
+        names = data[name_start_idx:name_end_idx]
+        values = data[values_start_idx:values_end_idx]
+        
+        # Gather keys and values
+        name_list = []
+        value_list = []
+        for i in range(len(names)):
+            name_entries = [entry.replace(" ","").replace("'", "").replace("BENNU_", "").replace("...", "") for entry in names[i].split("' '")]
+            value_entries = values[i].split(" ")
+            value_entries = np.unique(value_entries).tolist()
+            value_entries.remove("")
+            try:
+                value_entries.remove('...\n')#[3:-1]
+            except:
+                value_entries.remove('];\n')
+            name_list = np.concatenate((name_list, name_entries))
+            value_list = np.concatenate((value_list, value_entries))
+
+        # Standardize formatting and populate data array
+        data_matrix = np.zeros((max_deg*(max_deg+1) - 2*(2+1), 6))
+        for k in range(len(name_list)):
+            name = name_list[k]
+            if "J" in name:
+                degree = name.split("J")[1]
+                if len(degree) > 1:
+                    name_list[k] = "C" + degree + "00"
+                else:
+                    name_list[k] = "C0" + degree + "00"
+            if "GM" in name:
+                continue
+            
+            coef = name_list[k][0]
+            i = int(name_list[k][1:3])
+            j = int(name_list[k][3:5])
+            value = float(value_list[k])
+
+            if coef == "C":
+                data_matrix[(i-2)*max_deg + j] = np.array([i, j, value, data_matrix[(i-2)*max_deg + j, 3], 0.0, 0.0])
+            else:
+                data_matrix[(i-2)*max_deg + j] = np.array([i, j, data_matrix[(i-2)*max_deg + j, 2], value, 0.0, 0.0])
+
+        # Remove empty rows 
+        data_matrix=data_matrix[~np.all(data_matrix == np.array([0,0,0,0,0,0]),axis=1)]
+
+        # Write data to processed file
+        with open(new_name, 'w') as f:
+            f.write("    %f    %f    %f    %d\n" % (self.radius, self.mu, 0.0, 16))
+            f.write("    0\t0\t1.00000000000E+00\t0.00000000000E+00\t0.00000000000E+00\t0.00000000000E+00\n")
+            f.write("    1\t0\t0.00000000000E+00\t0.00000000000E+00\t0.00000000000E+00\t0.00000000000E+00\n")
+            f.write("    1\t1\t0.00000000000E+00\t0.00000000000E+00\t0.00000000000E+00\t0.00000000000E+00\n")
+            for row in data_matrix:
+                f.write("\t%d\t%d\t%e\t%e\t%e\t%e \n" % (row[0], row[1], row[2], row[3],row[4],row[5]))
+        return new_name
+
+
 
 class Eros:
     def __init__(self):
@@ -164,32 +166,13 @@ class Eros:
         G = 6.67430 * 10 ** -11
         self.mu = G * 6.687 * 10 ** 15
 
-        def reindex_faces(fname, action, pooch_inst):
-            "add 1 to the face indices in the obj file to work with trimesh"
-            new_name = fname.split("_raw")[0] + ".obj"
-            if os.path.exists(new_name):
-                return new_name
-
-            with open(fname, 'r') as f:
-                lines = f.readlines()
-            
-            for i in range(len(lines)):
-                line = lines[i]
-                if line[0] == 'f':
-                    lines[i] = 'f ' + ' '.join([str(int(entry) + 1) for entry in line.split('f')[1].split()]) + " \n"
-            
-            with open(new_name, 'w') as f:
-                f.writelines(lines)
-                
-            return new_name
-
 
         self.obj_8k = pooch.retrieve(
             url='http://sbnarchive.psi.edu/pds3/near/NEAR_A_5_COLLECTED_MODELS_V1_0/data/msi/eros007790.tab',
             known_hash="183df4df96ea6c66dee7a4b2368dc706d81c4942fbfb043198260f5406233ff0",
             fname="eros_shape_7790_raw.obj",
             path=os.path.dirname(GravityModels.__file__) + "/Files/ShapeModels/Eros/",
-            processor=reindex_faces
+            processor=self.reindex_faces
         )
 
         self.obj_90k = pooch.retrieve(
@@ -197,7 +180,7 @@ class Eros:
             known_hash="15184730d0a79db5d4de600fed7c758b3beb148f50d4d0e0acbebe7a1f73d82f",
             fname="eros_shape_89398_raw.obj",
             path=os.path.dirname(GravityModels.__file__) + "/Files/ShapeModels/Eros/",
-            processor=reindex_faces
+            processor=self.reindex_faces
         )
 
         self.obj_200k = pooch.retrieve(
@@ -205,27 +188,51 @@ class Eros:
             known_hash="54c7bc73376022876a7522e002355a4046777d346fe99270c230fee92cea881f",
             fname="eros_shape_200700_raw.obj",
             path=os.path.dirname(GravityModels.__file__) + "/Files/ShapeModels/Eros/",
-            processor=reindex_faces
+            processor=self.reindex_faces
         )
-
-        # Spherical Harmonics
-        def format_sh(fname, action, pooch_inst):
-            with open(fname, 'r') as f:
-                data = f.read()
-            processed_name = fname.split("_raw.txt")[0] + ".txt"
-            with open(processed_name, 'w') as f:
-                f.write("    %f    %f    %f    %d\n" % (self.radius, self.mu, 0.0, 15))
-                f.write("    0    0  1.00000000000E+00  0.00000000000E+00  0.00000000000E+00  0.00000000000E+00\n")
-                f.write(data)
-            return processed_name
 
         self.sh_file = pooch.retrieve(
             url='http://sbnarchive.psi.edu/pds3/near/NEAR_A_5_COLLECTED_MODELS_V1_0/data/rss/n15acoeff.tab',
             known_hash="e08068e2ea5167bee685ae00a8596144964e1da71ab16c51f2328f642d0be90e",
             fname="eros_sh_N15A_raw.txt",
-            path=os.path.dirname(GravityModels.__file__) + "/Files/GravityModels/Eros/",
-            processor=format_sh
+            path=os.path.dirname(GravityModels.__file__) + "/Files/SH_coefficients/Eros/",
+            processor=self.format_sh
         )
+
+
+    def reindex_faces(self, fname, action, pooch_inst):
+        "add 1 to the face indices in the obj file to work with trimesh"
+        new_name = fname.split("_raw")[0] + ".obj"
+        if os.path.exists(new_name):
+            return new_name
+
+        with open(fname, 'r') as f:
+            lines = f.readlines()
+        
+        for i in range(len(lines)):
+            line = lines[i]
+            if line[0] == 'f':
+                lines[i] = 'f ' + ' '.join([str(int(entry) + 1) for entry in line.split('f')[1].split()]) + " \n"
+        
+        with open(new_name, 'w') as f:
+            f.writelines(lines)
+            
+        return new_name
+
+
+    # Spherical Harmonics
+    def format_sh(self, fname, action, pooch_inst):
+        with open(fname, 'r') as f:
+            data = f.read()
+        processed_name = fname.split("_raw.txt")[0] + ".txt"
+        with open(processed_name, 'w') as f:
+            f.write("    %f,    %f,    %f,    %d\n" % (self.radius, self.mu, 0.0, 15))
+            f.write("    0,    0,  1.00000000000E+00,  0.00000000000E+00,  0.00000000000E+00,  0.00000000000E+00\n")
+            lines = [x.strip() for x in data.split("\n")]
+            entries = [line.split() for line in lines]
+            write_data = [",".join(x) for x in entries]
+            f.writelines(write_data)
+        return processed_name
 
 
 
--- GravityModels/CelestialBodies/Planets.py
+++ GravityModels/CelestialBodies/Planets.py
@@ -10,71 +10,20 @@ class Earth:
         self.mu = 0.3986004415e15  # meters^3/s^2
         self.radius = 6378136.6  # meters
         
-
-        def unpack(fname, action, member):
-            # manual unpack processor
-            unzipped = fname + ".unzipped"
-            # Don't unzip if file already exists and is not being downloaded
-            if action in ("update", "download") or not os.path.exists(unzipped):
-                with ZipFile(fname, "r") as zip_file:
-                    # Extract the data file from within the archive
-                    with zip_file.open(member) as data_file:
-                        # Save it to our desired file name
-                        with open(unzipped, "wb") as output:
-                            output.write(data_file.read())
-            # Return the path of the unzipped file
-            return unzipped
-
-        def format_EGM96_sh(fname, action, pooch_inst):
-            unzipped = unpack(fname, action, "EGM96")
-            new_name = fname.split("_raw")[0] + ".txt"
-            if os.path.exists(new_name):
-                return new_name
-
-            with open(unzipped, 'rb') as f:
-                data = f.readlines()
-            os.remove(unzipped)
-
-            with open(new_name, 'w') as f:
-                f.write("%f,%f,%f,%d\n" % (self.radius, self.mu, 0.0, 360))
-                f.write("0,0, 1.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00\n")
-                f.write("1,0, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00\n")
-                f.write("1,1, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00\n")
-                f.writelines([ re.sub("\s+", ",", line.decode("utf-8").lstrip()) + "\n" for line in data])
-            return new_name
-
-        def format_EGM2008_sh(fname, action, pooch_inst):
-            unzipped = unpack(fname, action, "EGM2008_to2190_TideFree")
-            new_name = fname.split("_raw")[0] + ".txt"
-            if os.path.exists(new_name):
-                return new_name
-
-            with open(unzipped, 'rb') as f:
-                data = f.readlines()
-            os.remove(unzipped)
-
-            with open(new_name, 'w') as f:
-                f.write("%f,%f,%f,%d\n" % (self.radius, self.mu, 0.0, 2190))
-                f.write("0,0, 1.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00\n")
-                f.write("1,0, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00\n")
-                f.write("1,1, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00\n")
-                f.writelines([ re.sub("\s+", ",", line.decode("utf-8").replace("D", "E").lstrip()) + "\n" for line in data])
-            return new_name
-
         self.EGM96 = pooch.retrieve(
             url='https://earth-info.nga.mil/php/download.php?file=egm-96spherical',
             known_hash="1f21ab8151c1b9fe25f483a4f6b78acdbf5306daf923725017b83d87a5f33472",
             fname="EGM96_raw.zip",
-            path=os.path.dirname(GravityModels.__file__) + "/Files/GravityModels/Earth/",
-            processor=format_EGM96_sh
+            path=os.path.dirname(GravityModels.__file__) + "/Files/SH_coefficients/Earth/",
+            processor=self.format_EGM96_sh
         )
 
         self.EGM2008 = pooch.retrieve(
             url='https://earth-info.nga.mil/php/download.php?file=egm-08spherical',
             known_hash='65a9072f337f156e8cbd76ffd773f536e6fb0de18697ea6726ecdb790fac0fbd',
             fname="EGM2008_raw.zip",
-            path=os.path.dirname(GravityModels.__file__) + "/Files/GravityModels/Earth/",
-            processor=format_EGM2008_sh
+            path=os.path.dirname(GravityModels.__file__) + "/Files/SH_coefficients/Earth/",
+            processor=self.format_EGM2008_sh
         )
 
         self.shape_model = os.path.dirname(GravityModels.__file__) + "/Files/ShapeModels/Earth/Earth.obj"
@@ -83,36 +32,88 @@ class Earth:
         # self.sh_file = self.EGM96
         self.sh_file = self.EGM2008
 
+    def unpack(self, fname, action, member):
+        # manual unpack processor
+        unzipped = fname + ".unzipped"
+        # Don't unzip if file already exists and is not being downloaded
+        if action in ("update", "download") or not os.path.exists(unzipped):
+            with ZipFile(fname, "r") as zip_file:
+                # Extract the data file from within the archive
+                with zip_file.open(member) as data_file:
+                    # Save it to our desired file name
+                    with open(unzipped, "wb") as output:
+                        output.write(data_file.read())
+        # Return the path of the unzipped file
+        return unzipped
+
+    def format_EGM96_sh(self, fname, action, pooch_inst):
+        unzipped = self.unpack(fname, action, "EGM96")
+        new_name = fname.split("_raw")[0] + ".txt"
+        if os.path.exists(new_name):
+            return new_name
+
+        with open(unzipped, 'rb') as f:
+            data = f.readlines()
+        os.remove(unzipped)
+
+        with open(new_name, 'w') as f:
+            f.write("%f,%f,%f,%d\n" % (self.radius, self.mu, 0.0, 360))
+            f.write("0,0, 1.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00\n")
+            f.write("1,0, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00\n")
+            f.write("1,1, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00\n")
+            f.writelines([ re.sub("\s+", ",", line.decode("utf-8").lstrip()) + "\n" for line in data])
+        return new_name
+
+    def format_EGM2008_sh(self, fname, action, pooch_inst):
+        unzipped = self.unpack(fname, action, "EGM2008_to2190_TideFree")
+        new_name = fname.split("_raw")[0] + ".txt"
+        if os.path.exists(new_name):
+            return new_name
+
+        with open(unzipped, 'rb') as f:
+            data = f.readlines()
+        os.remove(unzipped)
+
+        with open(new_name, 'w') as f:
+            f.write("%f,%f,%f,%d\n" % (self.radius, self.mu, 0.0, 2190))
+            f.write("0,0, 1.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00\n")
+            f.write("1,0, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00\n")
+            f.write("1,1, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00, 0.00000000000E+00\n")
+            f.writelines([ re.sub("\s+", ",", line.decode("utf-8").replace("D", "E").lstrip()) + "\n" for line in data])
+        return new_name
+
+
+
 class Moon:
     def __init__(self):
         self.body_name = "moon"
         self.radius = 1738100.0  # meters
         self.mu = 4.902799e12  # meters^3/s^2
 
-        def format_sh(fname, action, pooch_inst):
-            new_name = fname.split("_raw.txt")[0] + ".txt"
-            if os.path.exists(new_name):
-                return new_name
-
-            with open(fname, 'r') as f:
-                data = f.readlines()
-            meta_data = np.array([float(x) for x in data[0].split(", ")])
-            meta_data[0] *= 1000.0 # change radius units to meters
-            meta_data[1] *= 1000.0**3 # change mu units to meters^3
-            
-            with open(new_name, 'w') as f:
-                f.write(np.array2string(meta_data, separator=', ', max_line_width=2000)[1:-1] + "\n")
-                f.write("    0,    0,  1.00000000000E+00,  0.00000000000E+00, 0.00000000000E+00,  0.00000000000E+00\n")
-                f.writelines(data[1:])
-            return new_name
-
         self.GRGM1200 = pooch.retrieve(
             url='https://pds-geosciences.wustl.edu/grail/grail-l-lgrs-5-rdr-v1/grail_1001/shadr/gggrx_1200a_sha.tab',
             known_hash="fa04c3dce9376948ad243f3df74144e2602f12d183ea4d179604ed0a79da7ded",
             fname="GRGM_1200_raw.txt",
-            path=os.path.dirname(GravityModels.__file__) + "/Files/GravityModels/Moon/",
-            processor=format_sh
+            path=os.path.dirname(GravityModels.__file__) + "/Files/SH_coefficients/Moon/",
+            processor=self.format_sh
         )
         
         self.sh_file = self.GRGM1200
         self.shape_model = os.path.dirname(GravityModels.__file__) + "/Files/ShapeModels/Moon/Moon.obj"
+
+    def format_sh(self, fname, action, pooch_inst):
+        new_name = fname.split("_raw.txt")[0] + ".txt"
+        if os.path.exists(new_name):
+            return new_name
+
+        with open(fname, 'r') as f:
+            data = f.readlines()
+        meta_data = np.array([float(x) for x in data[0].split(", ")])
+        meta_data[0] *= 1000.0 # change radius units to meters
+        meta_data[1] *= 1000.0**3 # change mu units to meters^3
+        
+        with open(new_name, 'w') as f:
+            f.write(np.array2string(meta_data, separator=', ', max_line_width=2000)[1:-1] + "\n")
+            f.write("    0,    0,  1.00000000000E+00,  0.00000000000E+00, 0.00000000000E+00,  0.00000000000E+00\n")
+            f.writelines(data[1:])
+        return new_name
